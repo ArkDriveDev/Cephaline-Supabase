f8966b0a0163e712c9a2557fc7cb355f43f5cf6a
Arky <20231736@nbsc.edu.ph>
2025-05-06 22:43:20 +0800

    added search bar.tsx and dynamically rendered at home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import Profile from '../components/profile';
+import Searchbar from '../components/SearchBar';
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Journals</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <Profile/>
-    <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-</div>
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Journals</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <Profile />
+                <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                    <Searchbar />
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
