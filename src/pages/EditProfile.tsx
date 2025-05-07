1c1f95ba589d16e98fe67c64639e00f30c638634
Arky <20231736@nbsc.edu.ph>
2025-05-07 08:33:30 +0800

    updated avatarupload.tsx

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,28 +1,35 @@
-import { 
+import {
     IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const EditProfile: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Template</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
-    );
+    IonContent,
+    IonHeader,
+    IonMenuButton,
+    IonPage,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
+import AvatarUploader from '../components/AvatarUploader';
+import React, { useRef, useState } from 'react';  
+
+const EditProfile: React.FC = () => {
+  const fileInputRef = useRef<HTMLInputElement| null>(null);
+  const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
+
+  const handleAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => setAvatarPreview(reader.result as string);
+      reader.readAsDataURL(file);
+    }
   };
-  
-  export default EditProfile;
+
+  return (
+    <AvatarUploader
+      avatarPreview={avatarPreview}
+      fileInputRef={fileInputRef}  
+      handleAvatarChange={handleAvatarChange}
+    />
+  );
+};
+
+export default EditProfile;
