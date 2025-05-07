b45d9895acf9a3caf1e6d7729d4667e1e06244f8
Arky <20231736@nbsc.edu.ph>
2025-05-07 21:04:16 +0800

    updated JournalPage.tsx as a parent

--- src/pages/JournalPage.tsx
+++ src/pages/JournalPage.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import {
   IonButtons,
   IonContent,
@@ -7,13 +8,12 @@ import {
   IonTitle,
   IonToolbar
 } from '@ionic/react';
-import PageTitle from '../components/JournalPage_omponents/PageTitle';
 import { useParams } from 'react-router-dom';
+import PageTitle from '../components/JournalPage_omponents/PageTitle';
 import Spectrum from '../components/JournalPage_omponents/Spectrum';
 import Journalized from '../components/JournalPage_omponents/Journalized';
 import Attachments from '../components/JournalPage_omponents/Attachements';
 import SavePage from '../components/JournalPage_omponents/SavePage';
-import { useState } from 'react';
 
 interface Attachment {
   type: string;
@@ -22,10 +22,31 @@ interface Attachment {
 
 const JournalPage: React.FC = () => {
   const { journalId } = useParams<{ journalId: string }>();
+  const [entry, setEntry] = useState('');
   const [attachments, setAttachments] = useState<Attachment[]>([]);
 
   const handleAttach = (attachment: Attachment) => {
-    setAttachments([...attachments, attachment]);
+    let attachmentText = '';
+    
+    switch(attachment.type) {
+      case 'link':
+        attachmentText = `\n[Link: ${attachment.content}]\n`;
+        break;
+      case 'image':
+        attachmentText = `\n[Image attached]\n`;
+        break;
+      case 'file':
+        attachmentText = `\n[File: ${attachment.content}]\n`;
+        break;
+      case 'folder':
+        attachmentText = `\n[Folder: ${attachment.content}]\n`;
+        break;
+      default:
+        attachmentText = `\n[Attachment]\n`;
+    }
+
+    setEntry(prevEntry => prevEntry + attachmentText);
+    setAttachments(prev => [...prev, attachment]);
   };
 
   return (
@@ -38,13 +59,20 @@ const JournalPage: React.FC = () => {
           <IonTitle>Journal {journalId}</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      
+      <IonContent className="ion-padding">
         <p>Viewing journal entry {journalId}</p>
         <PageTitle />
-        <h1 style={{ margin: '30px', marginTop: '80px' }}>How are you feeling for these Journal?</h1>
+        <h1 style={{ margin: '30px 0' }}>How are you feeling about this journal?</h1>
         <Spectrum />
-        <Journalized attachments={attachments} />
-        <div style={{ display: 'flex', alignItems: 'center', gap: '1590px', marginTop: '-18px' }}>
+        <Journalized entry={entry} onEntryChange={setEntry} />
+        
+        <div style={{ 
+          display: 'flex', 
+          justifyContent: 'space-between', 
+          alignItems: 'center',
+          marginTop: '20px'
+        }}>
           <Attachments onAttach={handleAttach} />
           <SavePage />
         </div>
