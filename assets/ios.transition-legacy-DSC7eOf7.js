5cd2d967328de6626cc3cf242b118d5d6cb59622
Arky <20231736@nbsc.edu.ph>
2025-05-08 13:39:32 +0800

    Updates

--- assets/ios.transition-legacy-DSC7eOf7.js
+++ assets/ios.transition-legacy-DSC7eOf7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D07cGn2F.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DJ6qXSpG.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
