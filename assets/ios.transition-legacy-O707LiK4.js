ba0dc96ae5d4b08523aa1c4c3cec893df9650e71
JethroBalaga <balagajet@gmail.com>
2025-05-23 12:30:36 +0800

    Updates

--- assets/ios.transition-legacy-O707LiK4.js
+++ assets/ios.transition-legacy-O707LiK4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Ccust7SO.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Btwm_vBt.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
