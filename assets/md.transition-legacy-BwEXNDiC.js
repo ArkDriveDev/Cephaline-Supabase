db0d510690cbd917209098b7ea4b0624520d3ea5
JethroBalaga <balagajet@gmail.com>
2025-05-24 19:11:09 +0800

    Updates

--- assets/md.transition-legacy-BwEXNDiC.js
+++ assets/md.transition-legacy-BwEXNDiC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CgW-X3za.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-H4OoQl-R.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
