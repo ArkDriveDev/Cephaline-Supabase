52c0d77241821ba199bb1579da28c088731d27dd
JethroBalaga <balagajet@gmail.com>
2025-05-24 20:07:04 +0800

    Updates

--- assets/ios.transition-legacy-Bms8ifm0.js
+++ assets/ios.transition-legacy-Bms8ifm0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-oKaoMv28.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BCXSQw70.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
