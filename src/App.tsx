98aac14fa6cb6e99618d54d1757905a342811131
Arky <20231736@nbsc.edu.ph>
2025-05-26 20:53:06 +0800

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,9 @@ import './theme/variables.css';
 /* Page imports */
 import Login from './pages/Login';
 import Registration from './pages/Register';
-import ForgotPassword from './pages/Forgotpassword';
 import ChangePass from './pages/Changepass';
 import Menu from './pages/Menu';
+import ForgotPassword from './pages/Forgotpassword';
 
 
 setupIonicReact();
