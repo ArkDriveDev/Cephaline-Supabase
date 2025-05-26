5715cd8b01ecf259f1cc857b4eee396fd9e49250
Arky <20231736@nbsc.edu.ph>
2025-05-26 21:17:40 +0800

    set base to /

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,7 @@ import Login from './pages/Login';
 import Registration from './pages/Register';
 import ChangePass from './pages/Changepass';
 import Menu from './pages/Menu';
+import ForgotPassword from './pages/Forgotpassword';
 
 
 setupIonicReact();
@@ -35,6 +36,7 @@ const App: React.FC = () => (
         <IonRouterOutlet>
           <Route exact path="/Cephaline-Supabase" component={Login} />
           <Route exact path="/Cephaline-Supabase/registration" component={Registration} />
+          <Route exact path="/Cephaline-Supabase/forgotpassword" component={ForgotPassword} />
           <Route exact path="/Cephaline-Supabase/changepassword" component={ChangePass} />
           <Route path="/Cephaline-Supabase/app" component={Menu} />
         </IonRouterOutlet>
